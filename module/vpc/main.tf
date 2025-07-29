79dbdc3c73c6c334302ac7e1f29676ca3570e640
Ihor Holinei <ihorholinei@MacBook-Air-2.local>
2025-07-29 15:01:38 -0500

    added route table and igw

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -30,3 +30,33 @@ resource "aws_subnet" "private" {
     Name = "private-subnet-${count.index + 1}"
   }
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_for_eks.id
+
+  tags = {
+    Name = "igw-vpc_eks"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc_for_eks.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Associate public subnets with the public route table
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
